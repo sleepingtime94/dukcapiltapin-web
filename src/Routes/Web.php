aa4e8e642222bbc2d2aed2e7af87b6d25a0696c2
Madhana <sleepingtime94@gmail.com>
2026-01-07 10:14:59 +0800

    refactor

--- src/Routes/Web.php
+++ src/Routes/Web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Bramus\Router\Router;
+use App\Controllers\ViewController;
 
 $router = new Router();
 $router->setNamespace('\App\Controllers');
 $router->get('/', 'ViewController@home');
-
 $router->get('/profil/([a-z0-9_-]+)', 'ViewController@preview');
 $router->get('/pelayanan/([a-z0-9_-]+)', 'ViewController@preview');
 $router->get('/publikasi/([a-z0-9_-]+)', 'ViewController@preview');
